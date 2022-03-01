c0c2e6b848a59a5db6d6ebef12830bb273b5a2b4
Avinash Gupta <gavinash6290@gmail.com>
2022-03-01 22:58:13 +0530

    Moving to deployment

---
+++ main.py
@@ -0,0 +1,34 @@
+
+from pushbullet import PushBullet
+from bs4 import BeautifulSoup
+import requests
+
+def __main__():
+  codechef_link = "https://www.codechef.com/"
+  pushBullet_access_token = "o.96jyHv9X9XHS8pi1YgMW8pWBNyrb6B0V"
+
+  html_content = requests.get(codechef_link).text
+  soup = BeautifulSoup(html_content, 'lxml')
+
+  contest_list = soup.find_all('div', class_ = "m-navigation-item__container")[1].find_all('a', class_ = "m-navigation-dropdown__item")
+  
+  upcoming_contests = []
+
+  for index, link in enumerate(contest_list):
+    if(link.span.text == "All Contests" or link.span.text == "Contest Calender"):
+      break
+    upcoming_contests.append(link)
+
+  msg = ""
+  for contest in upcoming_contests:
+    msg += (f"{contest.span.text} : {codechef_link}{contest['href']}\n\n")
+
+  if msg == "":
+    msg = "No contest has been scheduled"
+  
+  pb = PushBullet(pushBullet_access_token)
+  push = pb.push_note("Contest Reminder", msg)
+
+  
+
+__main__()
